c48e3f05c5c5dc81ae6bfad0ec79061e386f41f5
Hackim7200 <hikmathakim7200@gmail.com>
2026-09-22 12:50:35 +0100

    creating a cicd pipeline

---
+++ app.py
@@ -0,0 +1,20 @@
+# app.py
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  
+    return 'Hello, World!'
+
+@app.route('/count')
+def count():
+     
+    return 'Count page!'
+
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
