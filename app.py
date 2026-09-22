7837f5cc7e13d70cdb99d0ed56130e2832156dc4
Hackim7200 <hikmathakim7200@gmail.com>
2026-09-22 13:53:49 +0100

    made some changes to python code

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ def count():
      
     return 'Count page!'
 
-  
+@app.route('/somethingelse')
+def something_else():
+     
+    return 'Something else page!'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
